b09c5131dbb1dadea2cd17f10d4babf8775a9905
Trenton Scott <InsaneGriot@Outlook.com>
2025-05-09 18:38:10 -0400

    Message Box

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,7 +29,17 @@ public class SimpleMath
     public static double Substraction(double num1, double num2) => num1 - num2;
 
     public static double Multiply(double num1, double num2) => num1 * num2;
-    public static double Divide(double num1, double num2) => num1 / num2;
+    public static double Divide(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            MessageBox.Show("Division by 0 is not supported", "Wrong operation", 
+                MessageBoxButton.OK, MessageBoxImage.Error);
+            return 0;
+        }
+
+        return num1 / num2;
+    }
 }
 public partial class MainWindow : Window
 {
