ce0fb0aafbd1217e85d69a34fa68fc8cbb6f9a98
Trenton Scott <InsaneGriot@Outlook.com>
2025-05-08 22:44:32 -0400

    Event Handler

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,8 +16,54 @@ namespace GenericCalculator;
 /// </summary>
 public partial class MainWindow : Window
 {
+    double lastNumber, result;
     public MainWindow()
     {
         InitializeComponent();
+
+        ACButton.Click += ACButton_Click;
+        NegativeButton.Click += NegativeButton_Click;
+        PercentangeButton.Click += PercentangeButton_Click;
+        EqualButton.Click += EqualButton_Click;
+    }
+
+    private void EqualButton_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void PercentangeButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+        {
+            lastNumber = lastNumber / 100;
+            resultLabel.Content = lastNumber.ToString();
+        }
+    }
+
+    private void NegativeButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+        {
+            lastNumber = lastNumber * -1;
+            resultLabel.Content = lastNumber.ToString();
+        }
+    }
+
+    private void ACButton_Click(object sender, RoutedEventArgs e)
+    {
+        resultLabel.Content = "0";
+    }
+
+    private void SevenButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (resultLabel.Content.ToString() == "0")
+        {
+            resultLabel.Content = "7";
+        }
+        else
+        {
+            resultLabel.Content = $"{resultLabel.Content}7";
+        }
     }
 }
