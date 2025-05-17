f8b59e2da22b68dc0238c50b9820049b918cf13d
Trenton Scott <InsaneGriot@Outlook.com>
2025-05-17 00:01:30 -0400

    Installer

--- obj/Debug/net8.0-windows/App.g.i.cs
+++ obj/Debug/net8.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace GenericCalculator {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace GenericCalculator {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/GenericCalculator;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GenericCalculator;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace GenericCalculator {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
         public static void Main() {
             GenericCalculator.App app = new GenericCalculator.App();
             app.InitializeComponent();
--- obj/Debug/net8.0-windows/GenericCalculator.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/GenericCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GenericCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19891c539708ebcb949ef3b3b4b787d687c2e69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+585df5375047073129e1680ba9a1debd314cc083")]
 [assembly: System.Reflection.AssemblyProductAttribute("GenericCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GenericCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
